7ea8af0cc06135e7c6009cbc5cc89e7dc20d4e62
fwgary <gkent7094@gmail.com>
2025-06-14 18:46:19 -0400

    Add routes/auth.js 1749941179493

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+router.get('/discord', passport.authenticate('discord'));
+
+router.get('/discord/callback', passport.authenticate('discord', {
+  failureRedirect: '/'
+}), (req, res) => {
+  res.redirect('/dashboard');
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
